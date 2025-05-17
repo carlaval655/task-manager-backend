16633e2d0bf6f0e1ef3550153fe5f6ea0f9373b2
carlaval655 <carla.val655@gmail.com>
2025-05-17 19:50:09 -0400

    Conf de bdd

--- config/db.js
+++ config/db.js
@@ -1,9 +1,21 @@
-const { Sequelize } = require('sequelize');
 require('dotenv').config();
+const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-  host: process.env.DB_HOST,
-  dialect: 'postgres',
-});
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false // Render requiere SSL pero no necesita verificación de cert
+      }
+    }
+  }
+);
 
 module.exports = sequelize;
